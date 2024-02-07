023801e6b4324caead8c71b63d85cb7b0c6c44bd
Alejandra Bedoya <alejandrabedoya00@gmail.com>
2024-02-07 14:42:51 -0500

    Skipping home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
-import React from "react";
+'use client'
+import React, { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      window.location.href = "/articles";
+    }, 100);
+
+    return () => clearTimeout(timeoutId);
+  }, []);
+
+
+
   return <div className="mt-10">Home</div>;
 }
